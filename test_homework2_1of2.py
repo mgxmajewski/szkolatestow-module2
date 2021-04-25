40b202e9567d3113699b39e63fbdf67c0ea88831
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-25 16:00:12 +0200

    Create TestClass Name TestClass required to follow 'Conventions for Python test discovery'

--- test_homework2_1of2.py
+++ test_homework2_1of2.py
@@ -2,24 +2,25 @@ from homework2_1of2 import VatService, Product
 import uuid
 from assertpy import assert_that
 
+class TestClass:
 
-def test_get_gross_price_for_default_vat():
-    # Given
-    vatService = VatService()
-    vatRate = 20.00
-    product = Product(uuid, vatRate)
-    # When
-    result = vatService.getGrossPriceForDefaultVat(product)
-    # Then
-    assert_that(result).is_equal_to(24.60)
+    def test_get_gross_price_for_default_vat(self):
+        # Given
+        self.vatService = VatService()
+        self.vatRate = 20.00
+        product = Product(uuid, self.vatRate)
+        # When
+        result = self.vatService.getGrossPriceForDefaultVat(product)
+        # Then
+        assert result == 24.60
 
 
-def test_get_gross_price():
-    # Given
-    vatService = VatService()
-    vatRate = 10.00
-    product = Product(uuid, vatRate)
-    # When
-    result = vatService.getGrossPrice(product.getNetPrice(), 0.08)
-    # Then
-    assert_that(result).is_equal_to(10.8)
+    def test_get_gross_price(self):
+        # Given
+        self.vatService = VatService()
+        self.vatRate = 10.00
+        product = Product(uuid, self.vatRate)
+        # When
+        result = self.vatService.getGrossPrice(product.getNetPrice(), 0.08)
+        # Then
+        assert result == 10.80
