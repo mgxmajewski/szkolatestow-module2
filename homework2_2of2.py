2522ada70ccae2de0c876f0ad0638af000fda7c2
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-26 23:27:31 +0200

    Correct getting default vat

--- homework2_2of2.py
+++ homework2_2of2.py
@@ -3,7 +3,7 @@ class VatService:
         self.vat_provider = vat_provider
 
     def get_gross_price_for_default_vat(self, product) -> float:
-        return self.calculate_gross_price(product.get_net_price(), self.vatValue)
+        return self.calculate_gross_price(product.get_net_price(), self.vat_provider.get_default_vat())
 
     def get_gross_price(self, net_price, product_type) -> float:
         vat_value = self.vat_provider.get_vat_for_type(product_type)
