9aace4144ff2349e7f67d0052798e8cb32b3bdb1
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-28 00:00:43 +0200

    Add fix test_get_gross_price test

--- test_homework2_2of2.py
+++ test_homework2_2of2.py
@@ -30,9 +30,10 @@ class TestClass:
         product = self.get_product_with_price(10.00, 'book')
         self.vat_provider.get_default_vat.return_value = 0.08
         # When
-        result = self.vat_service.get_gross_price_for_default_vat(product)
+        result = self.vat_service.get_gross_price(product.get_net_price(), product.product_type)
         # Then
         assert_that(result).is_equal_to(10.80)
+
 
 
 
