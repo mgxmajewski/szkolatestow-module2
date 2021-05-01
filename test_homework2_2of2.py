5bca037a7ec6d8be7d769a8e45da5a87459d79c7
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-01 23:28:25 +0200

    Add test for raising exception on string vat value

--- test_homework2_2of2.py
+++ test_homework2_2of2.py
@@ -30,9 +30,24 @@ class TestClass:
         product = self.get_product_with_price(10.00, 'book')
         self.vat_provider.get_vat_for_type.return_value = 0.08
         # When
-        result = self.vat_service.get_gross_price(product.get_net_price(), product.get_product_type())
+        result = self.vat_service.get_gross_price(product.get_net_price(),
+                                                  product.get_product_type())
         # Then
         assert_that(result).is_equal_to(10.80)
+
+    def test_if_get_gross_price_raise_type_exception_on_string(self):
+        # Given
+        product = self.get_product_with_price(10.00, 'book')
+        self.vat_provider.get_vat_for_type.return_value = 'book'
+        # When
+        result = self.vat_service.get_gross_price
+        # Then
+        assert_that(result).raises(TypeError).when_called_with(product.get_net_price(),
+                                                               product.get_product_type())
+
+
+
+
 
 
 
